d56e9800d9e146d6996b573cb7ba545faa45ff17
dharmendra-sehgal <dharmendra.sehgal@decorist.com>
2020-03-13 14:08:08 +0530

    first commit singal sign on using og Angular, OAuth2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { JwksValidationHandler } from 'angular-oauth2-oidc-jwks';
+
+import { authCodeFlowConfig } from './sso.config';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sso-angular-app';
+  constructor(
+    private oauthService: OAuthService) {
+      this.configureSingleSignOn();
+  }
+  configureSingleSignOn(){
+    this.oauthService.configure(authCodeFlowConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+    //this.oauthService.loadDiscoveryDocumentAndLogin(); //redirect on Login view
+  }
+  login() {
+    this.oauthService.initLoginFlow();
+  }
+  logout() {
+    this.oauthService.logOut();
+  }
+
+  get token() {
+    let claims:any = this.oauthService.getIdentityClaims();
+    return claims ? claims: null;
+  }
 }
